2eb8749b1c7363415aafb576954f4132e6f76cdc
nin-jin <nin-jin@users.noreply.github.com>
2020-08-16 07:12:37 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.test.js
+++ node.test.js
@@ -4503,6 +4503,12 @@ var $;
                 color: $.$mol_theme.hover,
             }
         },
+        ':focus-within': {
+            outline: 'none',
+            background: {
+                color: $.$mol_theme.hover,
+            }
+        },
         '@': {
             mol_link_current: {
                 'true': {
