a14fa2ad37dd9347be719cd22b2df0b391eb13d1
nin-jin <nin-jin@users.noreply.github.com>
2022-09-08 07:35:06 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.d.ts
+++ node.d.ts
@@ -2233,7 +2233,7 @@ declare namespace $ {
             src: string;
             srcdoc: any;
             allow: string;
-            allowFullscreen: boolean;
+            allowfullscreen: boolean;
         };
         accelerometer(): boolean;
         autoplay(): boolean;
