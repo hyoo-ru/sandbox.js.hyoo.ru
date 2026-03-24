8119e19154436a16513fd5cd3d40326b62ff478a
nin-jin <nin-jin@users.noreply.github.com>
2026-03-24 07:56:09 +0000

    Deploying to gh-pages from @ hyoo-ru/sandbox.js.hyoo.ru@447415dea597decaa77518231be4eb4622725ffa 🚀

--- node.mjs
+++ node.mjs
@@ -6884,6 +6884,7 @@ var $;
 "use strict";
 var $;
 (function ($) {
+    let x = /x/[Symbol.matchAll];
     class $mol_regexp extends RegExp {
         groups;
         constructor(source, flags = 'gsu', groups = []) {
--- node.test.js
+++ node.test.js
@@ -6875,6 +6875,7 @@ var $;
 "use strict";
 var $;
 (function ($) {
+    let x = /x/[Symbol.matchAll];
     class $mol_regexp extends RegExp {
         groups;
         constructor(source, flags = 'gsu', groups = []) {
